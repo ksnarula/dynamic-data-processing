820414ceeadaf9a267fd8a461a7b66fcc38d59e8
Karan Narula <maltorf05@gmail.com>
2016-08-19 20:14:25 -0700

    Not sure if it's correct yet. Lots of room to mess up. Added a ton of code to the check methods and commented basically everything. Fingers crossed.

--- Libraries/P-Square/P-Square/DynamicPercentileCalculator.cs
+++ Libraries/P-Square/P-Square/DynamicPercentileCalculator.cs
@@ -8,31 +8,39 @@ namespace DynamicDataProcessing
 {
     class DynamicPercentileCalculator
     {
-        float[][] markers;        
-        private Stack<float> standardSettingValues;
-        private bool initialPhase;
-        private float percentile;
+        float[][] markers;      //Array to keep track of marker position and height.
+        private Stack<float> standardSettingValues;     //Initial stack to store the first 5 data values.
+        private bool initialPhase;      //Switch toggling between the first phase (just collection) and second phase (marker adjustment).
+        private float percentile;       //The user-defined percentile to calculate markers for.
 
         public DynamicPercentileCalculator(float percentile)
         {
             this.percentile = percentile;
 
-            markers = new float[5][];
+            markers = new float[5][];       //Default of 5 markers used when calculating quantiles as per the P-Square Algorithm.
             standardSettingValues = new Stack<float>();
             
             initialPhase = true;
         }
 
-        public void addObservation(float observation)
+        /// <summary>
+        /// Add the latest data value to the class and readjust all marker placements and heights.
+        /// </summary>
+        /// <param name="observation">Float value of the latest data observation.</param>
+        /// <returns>Height of the adjusted marker representing the percentile value for the dataset. -1 if there is not enough data to calculate.</returns>
+        public float addObservation(float observation)
         {
-            if (initialPhase)
+            switch (initialPhase)
             {
-                phase1Controller(observation);
+                case true:      //Check whether the first 5 values have been added yet. If not, then initialPhase == true.
+                    phase1Controller(observation);
+                    break;
+
+                case false:    //If initialPhase == false then call the controller method for Phase 2 (adjusting markers).
+                    return phase2Controller(observation);
             }
-            else
-            {
-                phase2Controller(observation);
-            }
+
+            return -1;      //Return -1 if still in the initial phase and all markers have not yet been placed.
         }
 
         #region Controllers
@@ -51,6 +59,7 @@ namespace DynamicDataProcessing
                 }
 
                 Array.Sort(temp);
+
                 for (int i = 0; i < temp.Length; i++)
                 {
                     markers[i] = new float[] { i, temp[i] };
@@ -60,61 +69,121 @@ namespace DynamicDataProcessing
                 return;
         }
 
-        private void phase2Controller(float observation)
+        private float phase2Controller(float observation)
         {
             //Check min.
             if(markers[0][1] > observation)
             {
                 markers[0][1] = observation;    //Set height to new min. No need to shift marker.
+
+                //Increment all marker positions to the right by one
+                for (int i = 1; i <= markers.Length; i++)
+                {
+                    markers[i][0] = markers[i][0] + 1;  
+                }
             }
+
             //Check max.
-            else if (markers[markers.Length-1][1] < observation)
+            else if (markers[markers.Length-1][1] <= observation)
             {
                 markers[markers.Length - 1][0] = markers[markers.Length - 1][0] + 1;    //Move marker over one.
                 markers[markers.Length - 1][1] = observation;   //Set height to new max.
             }
+
             //Make internal adjustments if not an extreme value.
             else
             {
                 int index = markers.Length-1;
-                int category;
+
                 //Find index of first marker with greater height than observation.
                 for(int i = 0; i < markers.Length; i++)
                 {
                     if(markers[i][1] > observation)
                     {
                         index = i;
+                        break;
                     }
                 }
-
-                category = index - 1;
-
-                //Increment all markers to the right of observation by one unit EXCEPT for the max.
-                for(int i = index; i < markers.Length-1; i++)
+                
+                //Increment all markers to the right of observation by one unit (start at index).
+                for(int i = index; i < markers.Length; i++)
                 {
                     markers[i][0] = markers[i][0] + 1;
                 }
 
-                checkParabolicTrendOfMarkers();
+                //Check for ideal marker placement and adjust for parabolic trend if that is not present
+                for (int i = 1; i < markers.Length-1; i++)
+                {
+                    checkForIdealPlacement(i);
+                }
+                
 
             }
+
+            return markers[markers.Length/2][1];        //Return the value of the middle marker, representing the percentile value.
         }
         #endregion
 
 
         #region Check Methods
-        //All check methods return a bool indicating whether or not a correction was made.
-        private void checkParabolicTrendOfMarkers()
+        //Check whether the middle three markers are within 1 space of their ideal marker positions 
+        //If they are not, then adjust marker heights and placements so they conform to the assumed qudratic trend
+        private void checkForIdealPlacement(int markerIndex)
         {
-            for(int i = 1; i < markers.Length-3; i++)    //Start at the first internal value and move every three until the third from last internal value.
-            {
+            float n = markers[markers.Length - 1][0];    //Total number of data points (placement of the final marker).
+            float idealPlacement;
 
+            //Calculate ideal positions for the three internal markers
+            switch (markerIndex)
+            {
+                case 1:
+                    idealPlacement = ((n - 1) * percentile / 2) + 1;
+                    break;
+
+                case 2:
+                    idealPlacement = ((n - 1) * percentile) + 1;
+                    break;
+
+                case 3:
+                    idealPlacement = ((n - 1) * (1 + percentile) / 2) + 1;
+                    break;
+
+                default:
+                    return;
             }
+
+            float d = idealPlacement - markers[markerIndex][0];     //Calculate delta between ideal placement and real placement of marker
+
+            //If marker placement is not within one unit of ideal placement, then adjust its height value
+            if((d > 1) || (d < -1))
+            {
+                if(d > 1)
+                {
+                    d = 1; 
+                }
+                else if (d < -1)
+                {
+                    d = -1;
+                }
+                else
+                {
+                    return;
+                }
+                adjustForNonidealPlacement(markerIndex, d);
+            }
+
         }
 
-        private void checkForOverlappingMarkers()
+        private void adjustForNonidealPlacement(int markerIndex, int d)
         {
+            float height = markers[markerIndex][1];     //Get current marker height
+            float forwardMarker = markers[markerIndex+1][0], backwardMarker = markers[markerIndex - 1][0];      //Get adjacent marker placements
 
+            float a = d / (forwardMarker - backwardMarker);
+            float b = (markers[markerIndex][0] - markers[markerIndex - 1][0] + d) * (markers[markerIndex + 1][1] - height) / (markers[markerIndex + 1][0] - markers[markerIndex][0]);
+            float c = (markers[markerIndex + 1][0] - markers[markerIndex][0] - d) * (height - markers[markerIndex - 1][1]) / (markers[markerIndex][0] - markers[markerIndex - 1][0]);
+
+            markers[markerIndex][1] = height + (a * (b + c));
         }
         #endregion
     }
